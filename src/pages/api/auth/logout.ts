3b150d520b9893c2d1b393b960eff3a26f7769bf
Mochammad Farros Fatchur Roji <farros.alfatih@gmail.com>
2025-10-10 08:45:03 +0700

    delete page login

--- src/pages/api/auth/logout.ts
+++
@@ -1,6 +0,0 @@
-import type { APIRoute } from 'astro';
-
-export const GET: APIRoute = async ({ cookies, redirect }) => {
-  cookies.delete('auth-session', { path: '/' });
-  return redirect('/auth');
-};
